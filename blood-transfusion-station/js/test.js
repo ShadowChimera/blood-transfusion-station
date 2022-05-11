42962efbbd09024ca29630be5a7617ae47a1236f
Alexey Babenko <alexey22042001@gmail.com>
2022-05-12 01:35:23 +0300

    Updated the way of handling server responses

--- blood-transfusion-station/js/test.js
+++ blood-transfusion-station/js/test.js
@@ -1,4 +1,5 @@
-const REGISTRATION_PHP_URL = '/php/auth-module/registration.php'
+const TEST_PHP_URL = '/php/auth-module/registration.php'
+// const TEST_PHP_URL = '/php/test.php'
 let result_block = null
 
 document.addEventListener('DOMContentLoaded', main)
@@ -6,17 +7,17 @@ document.addEventListener('DOMContentLoaded', main)
 function main() {
     result_block = document.getElementById('common-server-answer')
 
-    // sendRequest()
+    sendRequest()
 }
 
 async function sendRequest() {
     const registration_data = new FormData()
 
-    registration_data.append('email', 'some@gmail.com')
-    registration_data.append('phone', '+380123456789')
+    registration_data.append('email', 'another@gmail.com')
+    registration_data.append('phone', '+380123456788')
     registration_data.append('password', 'test_password')
 
-    const response = await fetch(REGISTRATION_PHP_URL, {
+    const response = await fetch(TEST_PHP_URL, {
         method: 'POST',
         body: registration_data,
     })
@@ -28,16 +29,21 @@ async function sendRequest() {
         return
     }
 
-    const serverAnswer = await response.json()
+    const serverAnswers = await response.json()
     // const serverAnswer = await response.text()
-    console.log(serverAnswer)
+    // console.log(serverAnswer)
+    // return
+    console.log(serverAnswers)
 
-    if (!serverAnswer.status !== 'ok') {
+    result_block.innerHTML = ''
+    serverAnswers.forEach((serverAnswer) => {
+        if (!serverAnswer.status !== 'ok') {
+            console.log(serverAnswer.message)
+            result_block.innerHTML += serverAnswer.message
+            return
+        }
+
         console.log(serverAnswer.message)
-        result_block.innerHTML = serverAnswer.message
-        return
-    }
-
-    console.log(serverAnswer.message)
-    result_block.innerHTML = serverAnswer.message
+        result_block.innerHTML += serverAnswer.message
+    })
 }
